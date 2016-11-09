d9eb9786316bc689c7d27ac88d99b3c99fca7b07
Jeremy Astolfi <jeremyjastolfi@gmail.com>
2016-11-08 20:46:01 -0500

    Added PlayerMovementScript and PlayerPhysicsScript. Physics needs collision detection.

---
+++ Brawlitics/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementScript : MonoBehaviour {
+
+    [SerializeField]
+    private float f_playerSpeed;
+
+    [SerializeField]
+    private float f_jumpHeight;
+
+    [SerializeField]
+    private float f_gravity;
+
+    [SerializeField]
+    private float f_lerpTime;
+
+    [SerializeField]
+    private Vector2 v2_playerVelocity;
+
+    protected enum e_PlayerAction
+    {
+        Standing,
+        Jumping,
+        Ducking
+    }
+
+    protected e_PlayerAction pA_playerAction;
+
+	// Use this for initialization
+	void Start ()
+    {
+        v2_playerVelocity = Vector2.zero;
+
+        pA_playerAction = e_PlayerAction.Standing;
+
+        f_gravity = GetComponent<PlayerPhysicsScript>().GetGravity;
+
+        InitializeNullVariables();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        UserInput();
+        if(pA_playerAction == e_PlayerAction.Jumping)
+        {
+            ApplyGravity();
+        }
+
+        transform.position = Vector2.Lerp(transform.position, v2_playerVelocity, f_lerpTime);
+	}
+
+    private void UserInput()
+    {
+        v2_playerVelocity.x += Input.GetAxis("Horizontal") * f_playerSpeed * Time.deltaTime;
+
+        if(Input.GetKeyDown(KeyCode.Space) && (pA_playerAction == e_PlayerAction.Standing || pA_playerAction == e_PlayerAction.Ducking))
+        {
+            pA_playerAction = e_PlayerAction.Jumping;
+            v2_playerVelocity.y += f_jumpHeight;// * Time.deltaTime;
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        v2_playerVelocity.y -= f_gravity * Time.deltaTime;
+    }
+
+    private void InitializeNullVariables()
+    {
+        if (f_playerSpeed <= 0.0f) { f_playerSpeed = 5.0f; }
+        if (f_jumpHeight <= 0.0f) { f_jumpHeight = 5.0f; }
+        if (f_lerpTime <= 0.0f) { f_lerpTime = 0.5f; }
+    }
+}
---
+++ Brawlitics/Assets/Scripts/PlayerPhysicsScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPhysicsScript : MonoBehaviour {
+
+    [SerializeField]
+    protected float f_gravity;
+
+    // Use this for initialization
+    void Start ()
+    {
+        InitializeNullVariables();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void InitializeNullVariables()
+    {
+        if (f_gravity <= 0.0f) { f_gravity = 10.0f; }
+    }
+
+    public float GetGravity
+    {
+        get
+        {
+            return f_gravity;
+        }
+        set
+        {
+            f_gravity = value;
+        }
+    }
+}
